c038f304e32747e383e836ac615e127b94bf4c67
KatasonovYP <katasonov-yury@yandex.ru>
2022-02-23 14:35:12 +0300

    style custom input

---
+++ src/components/input/YInput.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import cl from './YInput.module.css'
+
+function YInput(props) {
+    return (  
+        <input {...props} className={cl.yInput} />
+    );
+}
+
+export default YInput;
