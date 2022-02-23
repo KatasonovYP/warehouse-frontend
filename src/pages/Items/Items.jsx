292edc1e6846b4e489b05a88591cd09a5a740e92
KatasonovYP <katasonov-yury@yandex.ru>
2022-02-23 14:04:55 +0300

    add testing page

--- src/pages/Items/Items.jsx
+++ src/pages/Items/Items.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import Header from "../../components/Header/Header/Header";
 import MainModal from './../../components/MainModal/MainModal';
+import MainBtn from './../../components/button/MainBtn/MainBtn';
 
 function Items() {
     return ( 
         <div>
-            <Header />
-            <h1>Items</h1>
-            <MainModal />
+            <Header/>
+                <h1>Items</h1>
+            <MainModal name='Вещи'>
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <div>
+                    <MainBtn>Да</MainBtn>
+                    <MainBtn>нет</MainBtn>
+                </div>
+            </MainModal>
         </div>
      );
 }
