89194c66d5d34e3e376e9597dd4b547804c279fa
KatasonovYP <katasonov-yury@yandex.ru>
2022-02-23 14:04:23 +0300

    add modal structure and styles

--- src/components/MainModal/MainModal.jsx
+++ src/components/MainModal/MainModal.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import cl from './MainModal.module.css'
 
 function MainModal(props) {
-    return (  
-        <div className={cl.mainModal}>
-            <div className={cl.header}>
-                <h3>{props.name}</h3>
-                <FontAwesomeIcon icon="fa-solid fa-xmark" />
+    return (
+        <div className={cl.modal__wrapper}>
+            <div className={cl.mainModal}>
+                <div className={cl.header}>
+                    <h3>{props.name}</h3>
+                    <i className="fa-solid fa-xmark"></i>
+                </div>
+                <div className={cl.body}>
+                    {props.children}
+                </div>
             </div>
-            {props.children}
         </div>
     );
 }
