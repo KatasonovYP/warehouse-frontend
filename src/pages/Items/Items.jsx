e9144dd37d8170835bc6a858789da311726012fa
KatasonovYP <katasonov-yury@yandex.ru>
2022-02-23 14:36:03 +0300

    import custom input

--- src/pages/Items/Items.jsx
+++ src/pages/Items/Items.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from "../../components/Header/Header/Header";
 import MainModal from './../../components/MainModal/MainModal';
 import MainBtn from './../../components/button/MainBtn/MainBtn';
+import YInput from '../../components/input/YInput';
 
 function Items() {
     return ( 
@@ -9,9 +10,9 @@ function Items() {
             <Header/>
                 <h1>Items</h1>
             <MainModal name='Вещи'>
-                <input type="text" />
-                <input type="text" />
-                <input type="text" />
+                <YInput type="text" placeholder='Name' />
+                <YInput type="text" placeholder='desctiption' />
+                <YInput type="text" placeholder='surname' />
                 <div>
                     <MainBtn>Да</MainBtn>
                     <MainBtn>нет</MainBtn>
