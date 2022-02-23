5ccd175f915bf7403c1f1620f299d903075f4316
KatasonovYP <katasonov-yury@yandex.ru>
2022-02-23 03:40:06 +0300

    create prototype task cards

---
+++ src/components/TaskCard/TaskCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import MainBtn from '../button/MainBtn/MainBtn'
+import cl from './TaskCard.module.css'
+
+function TaskCard({props, key}) {
+    return (
+        <div className={cl.TaskCard}>
+            <h3>{props.name}</h3>
+            <p>{props.address}</p>
+            <p>{props.customer}</p>
+            <MainBtn color='blue'>Info</MainBtn>
+        </div>
+    );
+}
+
+export default TaskCard;
--- src/pages/Tasks/Tasks.jsx
+++ src/pages/Tasks/Tasks.jsx
@@ -2,17 +2,46 @@ import React from 'react';
 import cl from './Tasks.module.css';
 import Header from '../../components/Header/Header/Header';
 import TaskArea from '../../components/TaskArea/TaskArea';
+import TaskCard from '../../components/TaskCard/TaskCard';
+import { useState } from 'react';
 
 function Tasks() {
+    const [tasks, setTasks] = useState([
+        {
+            id: 0,
+            name: 'Первый',
+            address: 'Baumanskaya 1',
+            customer: 'Ivanov',
+        },
+        {
+            id: 1,
+            name: 'Второй',
+            address: 'Baumanskaya 2',
+            customer: 'Alekseev',
+        },
+    ]);
+
     return (
         <div>
             <Header />
             <h1> задачи </h1>
             <p> Здесь будут ваши задачи</p>
             <div className={cl.TaskAreas}>
-                <TaskArea>asdf</TaskArea>
-                <TaskArea>asdfasdf</TaskArea>
-                <TaskArea>asdf</TaskArea>
+                <TaskArea>
+                    {tasks.map((task) => (
+                        <TaskCard props={task} key={task.id} />
+                    ))}
+                </TaskArea>
+                <TaskArea>
+                    {tasks.map((task) => (
+                        <TaskCard props={task} key={task.id} />
+                    ))}
+                </TaskArea>
+                <TaskArea>
+                    {tasks.map((task) => (
+                        <TaskCard props={task} key={task.id} />
+                    ))}
+                </TaskArea>
             </div>
         </div>
     );
