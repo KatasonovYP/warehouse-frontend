001fc2349ed8f8325f45d0ac9caf2e6b3dd435c6
KatasonovYP <katasonov-yury@yandex.ru>
2022-02-23 18:14:31 +0300

    add new routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import './styles/App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Tasks from './pages/Tasks/Tasks';
+import SignIn from './pages/SignIn/SignIn';
+import SignUp from './pages/SignUp/SignUp';
 import Storages from './pages/Storages/Storages';
+import Tasks from './pages/Tasks/Tasks';
 import Items from './pages/Items/Items';
 
 function App() {
     return (
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Tasks />} />
-            <Route path="/storages" element={<Storages />} />
-            <Route path="/items" element={<Items />} />
-          </Routes>
+            <Routes>
+                <Route path='/' element={<Tasks />} />
+                <Route path='/sign_in' element={<SignIn />} />
+                <Route path='/sign_up' element={<SignUp />} />
+                <Route path='/storages' element={<Storages />} />
+                <Route path='/items' element={<Items />} />
+            </Routes>
         </BrowserRouter>
-      );
+    );
 }
 
 export default App;
