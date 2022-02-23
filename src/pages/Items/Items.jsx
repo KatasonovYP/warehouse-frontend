6c83a2f0086113fbbb7c4e51d75f276065679181
KatasonovYP <katasonov-yury@yandex.ru>
2022-02-23 15:37:19 +0300

    add modal to items page

--- src/pages/Items/Items.jsx
+++ src/pages/Items/Items.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { useState } from 'react';
 import Header from "../../components/Header/Header/Header";
 import MainModal from './../../components/MainModal/MainModal';
 import MainBtn from './../../components/button/MainBtn/MainBtn';
 import YInput from '../../components/input/YInput';
 
 function Items() {
+
+    const [modal, setModal] = useState(false)
+
     return ( 
         <div>
             <Header/>
                 <h1>Items</h1>
-            <MainModal name='Вещи'>
+            <MainBtn color='blue' onClick={() => setModal(true)}>Create</MainBtn>
+            <MainModal name='Вещи' visible={modal} setVisible={setModal}>
                 <YInput type="text" placeholder='Name' />
                 <YInput type="text" placeholder='desctiption' />
                 <YInput type="text" placeholder='surname' />
