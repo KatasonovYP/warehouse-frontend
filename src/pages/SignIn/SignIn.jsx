816676b791b1085e5362d4af4c694a7b42007aa0
KatasonovYP <katasonov-yury@yandex.ru>
2022-02-23 18:13:33 +0300

    create singIn page

---
+++ src/pages/SignIn/SignIn.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Auth from '../../components/Auth/Auth';
+
+function SignIn() {
+    return (
+        <Auth
+            title='Авторизация'
+            inputs={[
+                { id: 0, type: 'text', placeholder: 'Логин' },
+                { id: 1, type: 'password', placeholder: 'Пароль' },
+            ]}
+            btnText='Авторизироваться'
+            path='/'
+        />
+    );
+}
+
+export default SignIn;
