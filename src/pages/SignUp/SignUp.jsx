01d595d930a3a685711756004dac3739eb0528d8
KatasonovYP <katasonov-yury@yandex.ru>
2022-02-23 18:14:00 +0300

    create SignUp page

---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Auth from '../../components/Auth/Auth';
+
+function SignUp() {
+    return (  
+        <Auth
+            title='Зарегестрироваться'
+            inputs={[
+                { id: 0, type: 'text', placeholder: 'Логин' },
+                { id: 1, type: 'email', placeholder: 'email' },
+                { id: 2, type: 'password', placeholder: 'Пароль' },
+            ]}
+            btnText='Зарегестрироваться'
+            path='/'
+        />
+    );
+}
+
+export default SignUp;
