8cfd02bff93ab8c891b8dac087e7d8b62f24ce9e
KatasonovYP <katasonov-yury@yandex.ru>
2022-03-01 02:16:52 +0300

    update item card style

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import cl from './ItemCard.module.css'
+import CountBtn from '../button/CountBtn/CountBtn';
+import cl from './ItemCard.module.css';
 
-function ItemCard({props}) {
-    return (  
-        <div>
+function ItemCard({ props }) {
+    return (
+        <div className={cl.itemCard}>
+            <input type="checkbox" />
             <h3>{props.title}</h3>
             <p>{props.id}</p>
+            <div>
+                <CountBtn>+</CountBtn>
+                <CountBtn>-</CountBtn>
+            </div>
         </div>
     );
 }
---
+++ src/components/button/CountBtn/CountBtn.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import cl from './CountBtn.module.css';
+
+function BtnBlue({children, ...props}) {
+    return (
+        <button className={cl.btn + ' ' + cl.blue} {...props}>
+            {children}
+        </button>
+    );
+}
+
+export default BtnBlue;
