381ec51c83ec45ff5c7ab30f4b4661fa30e86141
KatasonovYP <katasonov-yury@yandex.ru>
2022-03-01 04:21:27 +0300

    create post request for new good:

--- src/pages/Items/Items.jsx
+++ src/pages/Items/Items.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 import Header from '../../components/Header/Header/Header';
 import MainModal from './../../components/MainModal/MainModal';
 import MainBtn from './../../components/button/MainBtn/MainBtn';
@@ -18,6 +19,22 @@ function Items() {
             .then((responses) => setItems(responses));
     }, []);
 
+    function postItem(body) {
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+        fetch(`http://127.0.0.1:8000/api/goods`, options)
+            .then((response) => response.json())
+            .then((responses) => console.log(responses));
+    }
+
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data) => postItem(data);
+
     return (
         <div>
             <Header />
@@ -30,22 +47,34 @@ function Items() {
                     <ItemCard key={item.id} props={item} />
                 ))}
             </div>
-            <MainModal name='Вещи' visible={modal} setVisible={setModal}>
-                <YInput type='text' placeholder='id' />
-                <YInput type='text' placeholder='title' />
-                <YInput type='text' placeholder='text' />
-                <YInput type='text' placeholder='price' />
+            <MainModal
+                name='Вещи'
+                visible={modal}
+                setVisible={setModal}
+                onSubmit={handleSubmit(onSubmit)}
+            >
+                <input type='text' placeholder='id' {...register('id')} />
+                <input type='text' placeholder='title' {...register('title')} />
+                <input type='text' placeholder='text' {...register('text')} />
+                <input type='text' placeholder='price' {...register('price')} />
+                <select {...register('unit')}>
+                    <option value='kg'>kg</option>
+                    <option value='PCS'>PCS</option>
+                    <option value='g'>g</option>
+                </select>
                 <div>
                     <MainBtn
+                        type='submit'
                         color='blue'
-                        onClick={() => {
-                            let data = new FormData();
-                            console.log(data);
-                        }}
+                        onClick={() => setModal(false)}
                     >
                         Да
                     </MainBtn>
-                    <MainBtn color='white' onClick={() => setModal(false)}>
+                    <MainBtn
+                        type='button'
+                        color='white'
+                        onClick={() => setModal(false)}
+                    >
                         нет
                     </MainBtn>
                 </div>
