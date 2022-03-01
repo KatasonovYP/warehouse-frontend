3b5161b71e9fc0eabe92157a67c25388e6d9a127
KatasonovYP <katasonov-yury@yandex.ru>
2022-03-01 18:49:07 +0300

    add loader on items page

--- src/pages/Items/Items.jsx
+++ src/pages/Items/Items.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import Header from '../../components/Header/Header/Header';
-import MainBtn from './../../components/button/MainBtn/MainBtn';
-import ItemCard from './../../components/ItemCard/ItemCard';
 import CreateItemModal from './CreateItemModal';
+import ItemCard from './../../components/ItemCard/ItemCard';
+import MainBtn from './../../components/button/MainBtn/MainBtn';
+import Loader from './../../components/Loader/Loader';
+import useFetching from './../../hooks/useFetching';
 
 function Items() {
     const [items, setItems] = useState([]);
     const [modal, setModal] = useState(false);
+    const [fetchItems, isLoading, error] = useFetching(async () => {
+        await fetch(`http://127.0.0.1:8000/api/warehouses/${id.toString()}/goods`)
+            .then((response) => response.json())
+            .then((responses) => setItems(responses));
+    });
 
     const id = window.location.href.split('/')[4];
 
-    useEffect(() => {
-        fetch(`http://127.0.0.1:8000/api/warehouses/${id.toString()}/goods`)
-            .then((response) => response.json())
-            .then((responses) => setItems(responses));
-    }, []);
+    useEffect(() => fetchItems(), []);
 
     return (
         <div>
@@ -25,9 +28,11 @@ function Items() {
                 Create
             </MainBtn>
             <div>
-                {items.map((item) => (
-                    <ItemCard key={item.id} props={item} />
-                ))}
+                {isLoading ? (
+                    <Loader />
+                ) : (
+                    items.map((item) => <ItemCard key={item.id} props={item} />)
+                )}
             </div>
             <CreateItemModal
                 modal={modal}
